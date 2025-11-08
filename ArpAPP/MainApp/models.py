65468bf01bd47107e3181119120db10d88ed7a87
Game-On-Dp <gorbach0864213579@gmail.com>
2025-11-08 21:09:40 +0200

    add model of graph

--- ArpAPP/MainApp/models.py
+++ ArpAPP/MainApp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import uuid
+import os
+
 
 class Project(models.Model):
     Name = models.TextField(blank=True, null=True)
@@ -29,4 +32,21 @@ class Node(models.Model):
 
 
 
+def graph_image_upload_path(instance, filename):
 
+    ext = filename.split('.')[-1] if '.' in filename else 'png'
+    name = f"{uuid.uuid4().hex}.{ext}"
+
+    return os.path.join('graphs', f'project_{instance.project.pk}', name)
+
+
+class GraphImage(models.Model):
+    project = models.ForeignKey('Project', on_delete=models.CASCADE, related_name='graphs')
+    image = models.ImageField(upload_to=graph_image_upload_path)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Graph {self.pk} for project {self.project.pk} @ {self.created_at}"
